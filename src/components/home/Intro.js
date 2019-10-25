0f39318cfbad74faa6887a9692577c1e53741706
Skylar Soehl-Gutierrez <ssoehl@truefit.com>
2019-10-25 16:11:04 -0400

    added content for intro and portfolio sections

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -3,7 +3,7 @@ import React from "react"
 const Intro = () => {
     return (
         <div>
-            <h2>Welcome! I’m Skylar Soehl, a creative developer and visual designer based in Boston. I’m a current <a href="https://www.khoury.northeastern.edu" target="_blank">Northeastern University</a> Student working as a software engineering co-op for <a href="https://www.truefit.com/Home" target="_blank">True Fit</a>. In my spare time, I run all things design for <a href="https://www.instagram.com/nuwomenintech/">NU Women in Tech</a>, take on freelance projects, and develop personal initiatives.</h2>
+            <p>Welcome! I’m Skylar Soehl, a creative developer and visual designer based in Boston. I’m a current <a href="https://www.khoury.northeastern.edu" target="_blank">Northeastern University</a> Student working as a software engineering co-op for <a href="https://www.truefit.com/Home" target="_blank">True Fit</a>. In my spare time, I run all things design for <a href="https://www.instagram.com/nuwomenintech/">NU Women in Tech</a>, take on freelance projects, and develop personal initiatives.</p>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Intro from "../components/home/Intro"
+import Work from "../components/home/SelectedWork"
 
 const IndexPage = () => {
     return (
         <div>
             <h1>Skylar Soehl</h1>
             <Intro />
+            <Work />
         </div>
     )
 }
