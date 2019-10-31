c1f117ce082d17b6311c4566f40f453d77fe3bb4
Skylar Soehl-Gutierrez <ssoehl@truefit.com>
2019-10-31 01:03:35 -0400

    added resume and email link to bio, added ending msg svg

--- src/components/home/Bio.js
+++ src/components/home/Bio.js
@@ -4,6 +4,8 @@ import * as githubIcon from '../../images/social-icons/github.svg'
 import * as igIcon from '../../images/social-icons/instagram.svg'
 import * as linkedInIcon from '../../images/social-icons/linkedIn.svg'
 import * as dribbleIcon from '../../images/social-icons/dribble.svg'
+import * as end from '../../images/the-end.svg'
+import * as resume from '../../documents/skylar_soehl_resume.pdf'
 
 const Bio = () => {
     return (
@@ -19,14 +21,6 @@ const Bio = () => {
                 
             <div>
                 <img src={self} alt='This is a black and white (slighty out of focus) photo of myself I took on my phone. I am a woman with shoulder-length dark brown hair. I am very close to the camera with my phone covering half of my face.'></img>
-            </div>
-
-            <div>
-                <p>My interest in web design and coding began in middle school out of a desire to make my Tumblr blog as cool as the blogs of the platform’s anonymous web-coding gurus, whose HTML + CSS tutorials I followed obsessively. Today, I continue to immerse myself in my love for all things web through tackling projects that deal with interaction design and front-end development.</p>
-
-                <p>I was born on Long Island and proud to be raised in a woman-ran household. As a Latinx woman with immigrant heritage, I am fiercely passionate about diversity in tech, accessibility on the web, and hope to one day become an example for what women of color can accomplish in this industry.</p>
-
-                <p>During my downtime I like to paint, read in the Boston Public Gardens, loiter in museums, explore craft breweries, go to concerts, surf (on the log), and listen to Fugazi’s Instrument on repeat</p>
             </div>
 
             <ul>
@@ -51,6 +45,21 @@ const Bio = () => {
                     </a>
                 </li>
             </ul>
+
+            <div>
+                <p>My interest in web design and coding began in middle school out of a desire to make my Tumblr blog as cool as the blogs of the platform’s anonymous web-coding gurus, whose HTML + CSS tutorials I followed obsessively. Today, I continue to immerse myself in my love for all things web through tackling projects that deal with interaction design and front-end development.</p>
+
+                <p>I was born on Long Island and proud to be raised in a woman-ran household. As a Latinx woman with immigrant heritage, I am fiercely passionate about diversity in tech, accessibility on the web, and hope to one day become an example for what women of color can accomplish in this industry.</p>
+
+                <p>During my downtime I like to paint, read in the Boston Public Gardens, loiter in museums, explore craft breweries, go to concerts, surf (on the log), and listen to Fugazi’s Instrument on repeat</p>
+
+                <a href={resume} target="_blank">Résumé</a>
+                <a href="mailto:skysoehl@gmail.com" target="_blank">Hit my line</a>
+                
+            </div>
+
+            <img src={end}></img>
+
 
 
         </div>
