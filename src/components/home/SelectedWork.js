91bd842dbfc0ed9ab8296866c2f9d3b1bd4d9fd8
Skylar Soehl-Gutierrez <ssoehl@truefit.com>
2019-11-05 05:07:55 -0500

    added portfolio and gallery images

--- src/components/home/SelectedWork.js
+++ src/components/home/SelectedWork.js
@@ -16,10 +16,11 @@ const Work = () => {
             <div>
                 <ul>
                     <li><span>2.1</span><Link to="/Butter">Butter</Link></li>
-                    <li><span>2.2</span><Link to="/Kakuro">Kakuro</Link></li>
-                    <li><span>2.3</span><Link to="/Esfresco">Esfresco</Link></li>
+                    <li><span>2.2</span><Link to="/Nuwit">NUWIT</Link></li>
+                    <li><span>2.3</span><Link to="/Kakuro">Kakuro</Link></li>
                     <li><span>2.4</span><Link to="/Tough">Tough</Link></li>
-                    <li><span>2.5</span><Link to="/Etc">Etc</Link></li>
+                    <li><span>2.5</span><Link to="/Esfresco">Esfresco</Link></li>
+                    <li><span>2.6</span><Link to="/Etc">Etc</Link></li>
                 </ul>
             </div>
             
