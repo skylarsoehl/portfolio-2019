71fe454f37b106699bebce994a12d7bf220c99f4
Skylar Soehl-Gutierrez <ssoehl@truefit.com>
2019-11-06 19:37:33 -0500

    added site title styling and site container

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        typekit: {
+          id: 'ujc5bkl'
+        }
+      }
+    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import './layout.scss';
 import * as theEnd from '../images/the-end.svg'
+
+const SiteContainer = styled.div`
+    margin: 57px;
+`;
+
+const SiteTitle = styled.div`
+    font-family: 'Maelstrom';
+    font-size: 100px;
+    font-size: 60px;
+    line-height: 78px;
+    position: absolute;
+    left: 3%;
+    right: 53.96%;
+    top: 7%;
+    bottom: 97.82%;
+`;
 
 const Layout = (props) => {
     return (
-        <div>
-            <h1>Skylar Soehl</h1>
+        <SiteContainer>
+            <SiteTitle>SKYLAR SOEHL</SiteTitle>
             {props.children}
             <img src={theEnd} alt='This is the end of the page.'></img>
-        </div>
+        </SiteContainer>
     )
 }
 export default Layout
